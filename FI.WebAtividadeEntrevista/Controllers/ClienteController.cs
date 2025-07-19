1626cebf4981b9d5a941edb8e3c1a20bf8d96333
Emanoel <josemanoel.am@gmail.com>
2025-07-19 10:23:10 -0300

    Adicionar campo CPF ao fluxo de trabalho e à interface do cliente
    Introduz uma propriedade CPF para Cliente e ClienteModel, atualiza o DaoCliente para lidar com CPF em operações de inserção, atualização e listagem, e modifica a lógica do controlador para mapeamento de CPF. O formulário de registro do cliente agora inclui uma entrada de CPF. Observação: o DaoCliente apresenta problemas em que o CPF é tratado incorretamente como "Telefone"; isso precisa ser revisado.

--- FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
+++ FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
@@ -49,7 +49,8 @@ namespace WebAtividadeEntrevista.Controllers
                     Nacionalidade = model.Nacionalidade,
                     Nome = model.Nome,
                     Sobrenome = model.Sobrenome,
-                    Telefone = model.Telefone
+                    Telefone = model.Telefone,
+                    CPF = model.CPF
                 });
 
            
@@ -111,7 +112,8 @@ namespace WebAtividadeEntrevista.Controllers
                     Nacionalidade = cliente.Nacionalidade,
                     Nome = cliente.Nome,
                     Sobrenome = cliente.Sobrenome,
-                    Telefone = cliente.Telefone
+                    Telefone = cliente.Telefone,
+                    CPF = cliente.CPF
                 };
 
             
